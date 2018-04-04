4b7c131f38a33685908a6aaf5665fe50815eff2d
EAD2187 <ead2187@columbia.edu>
2018-04-03 23:17:24 -0400

    170403_commit to master

--- ASSIGNMENT4.1/geography.js
+++ ASSIGNMENT4.1/geography.js
@@ -1,8 +1,9 @@
-// var table;
-// var cost;
-// var time;
-// var county;
-// var margin = 50;
+var table;
+var cost;
+var time;
+var length;
+var margin = 50;
+var d;
 
 
 // function preload() {
@@ -15,108 +16,82 @@
 // function setup() {
 //   createCanvas(windowWidth, windowHeight);
 //   loadData();
+//   console.log('"load data"');
 //   noLoop();
+
+//   console.log("setup");
 // }
 
 // function loadData() {
 //     cost = table.getColumn("Cost");
 //     time = table.getColumn("Time");
-//     county = table.getColumn("County");
 //     console.log(cost);
 //     console.log(time);
-//     console.log(county);
 //   }
 
 //   function draw() {
 //   background(0,0,255,50);
 //   fill(0,255,0,20);
 
-//   for (var i = 0; i < cost.length; i++) {
-
-//         var j = 0; j < time.length; j++;
-
-//         if (county == 'BK'){
-//                                 fill (130, 130, 130, 30);
-//                         }
-//                         else if (county == 'BX') {
-//                                 fill (70, 178, 157, 30);
-//                         }
-//                         else if (county == 'K') {
-//                                 fill (146, 214, 195, 30);
-//                         }
-//                         else if (county == 'MN') {
-//                                 fill (19, 96, 74, 30);
-//                         }
-//                         else if (county == 'NY') {
-//                                 fill (219, 49, 70, 30);
-//                         } 
-//                         else if (county == 'Q') {
-//                                 fill (92,96, 161, 30);
-//                         }
-//                         else if (county == 'QN') {
-//                                 fill (4, 227, 229, 30);
-//                         }
-//                         else if (county == 'R') {
-//                                 fill (7, 166, 212, 30);
-//                         }
-//                         else if (county == 'ST') {
-//                                 fill (100, 6, 200, 30);
-//                         }
-
 //   ellipse(time[j], height/2, cost[i]/10);
-
-//   var d = int(dist(mouseX, mouseY, positionX, positionY));
-//                         if (d < 8){
-//                                 text('COST:', 50, 500);
-//                                 text(cost, 175, 500);
-//                                 text('COUNTY:', 50, 515);
-//                                 text(county, 175, 515);
-//                                 text('TIME:', 50, 530);
-//                                 text(time, 175, 530);
-//                                 noStroke();
-//                         }
-// }
-//   }
-
-
+// }// end for loop
+// //   var d = int(dist(mouseX, mouseY, positionX, positionY));
+// //                         if (d < 8){
+// //                                 text('COST:', 50, 500);
+// //                                 text(cost, 175, 500);
+// //                                 text('TIME:', 50, 530);
+// //                                 text(time, 175, 530);
+// //                                 noStroke();
+// //                         }
+// // }
 
 
 
 
 function preload(){
-    table = loadTable("parkingticketdata.csv", "csv", "header");
-    console.log(table);
+  table = loadTable("parkingticketdata.csv", "csv", "header");
+  console.log('table');
 
 }
 
 function setup(){
-    createCanvas(750, 750);
-    background (255);
-    textSize(20);
-    noLoop();
+  createCanvas(750, 750);
+  background (196,255,51);
+  textSize(20);
+  noLoop();
+  console.log('setup');
 
 }
 
 function loadData() {
-  var Cost;
-  var Time;
-  Cost = table.getColumn("Cost");
-  Time = table.getColumn("Time");
-  console.log(Cost);
-  console.log(timeout);
+  cost = table.getColumn("Cost");
+  time = table.getColumn("Time");
+  console.log('cost + time');
 }
 
 
 function draw(){
-    background(0);
+  background(196,255,51);
+  stroke(0,135,50,50);
 
+  cost = table.getColumn("Cost");
+  time = table.getColumn("Time");
 
-    for (var i=0;i<Cost.length; i++) {
-        fill('red');
-        var Xaxis = map (Time[i],0,1499,50,width-50)
-        var radius = map (Cost[i],0,515,0,50)
+  for (var i=0;i<cost.length; i++) {
+    fill(0,255,168,30);
+    var Xaxis = map (time[i],0,1499,50,width-50)
+    var radius = map (cost[i],0,515,0,50)
 
-        ellipse(height*0.5, Xaxis, radius,radius)
-    }
+    ellipse(height*0.5, Xaxis, radius,radius)
+  }
+
+    var d = int(dist(mouseX, mouseY, radius, radius));
+                        if (d < 8){
+                                text('COST:', 50, 500);
+                                text(cost, 175, 500);
+                                text('TIME:', 50, 530);
+                                text(time, 175, 530);
+                                noStroke();
+                        }
 
 }
---
+++ ASSIGNMENT4.1/geography_old2.js
@@ -0,0 +1,79 @@
+var table;
+var cost;
+var time;
+var county;
+var margin = 50;
+
+
+function preload() {
+        table = loadTable('parkingticketdata.csv', 'csv', 'header');
+        noLoop(); //draw background once; recommended for static graphs//
+        console.log('Table has been loaded...');
+}
+
+// In this sketch everything happens in setup
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  loadData();
+  noLoop();
+}
+
+function loadData() {
+    cost = table.getColumn("Cost");
+    time = table.getColumn("Time");
+    county = table.getColumn("County");
+    console.log(cost);
+    console.log(time);
+    console.log(county);
+  }
+
+  function draw() {
+  background(0,0,255,50);
+  fill(0,255,0,20);
+
+  for (var i = 0; i < cost.length; i++) {
+
+        var j = 0; j < time.length; j++;
+
+        if (county == 'BK'){
+                                fill (130, 130, 130, 30);
+                        }
+                        else if (county == 'BX') {
+                                fill (70, 178, 157, 30);
+                        }
+                        else if (county == 'K') {
+                                fill (146, 214, 195, 30);
+                        }
+                        else if (county == 'MN') {
+                                fill (19, 96, 74, 30);
+                        }
+                        else if (county == 'NY') {
+                                fill (219, 49, 70, 30);
+                        } 
+                        else if (county == 'Q') {
+                                fill (92,96, 161, 30);
+                        }
+                        else if (county == 'QN') {
+                                fill (4, 227, 229, 30);
+                        }
+                        else if (county == 'R') {
+                                fill (7, 166, 212, 30);
+                        }
+                        else if (county == 'ST') {
+                                fill (100, 6, 200, 30);
+                        }
+
+  ellipse(time[j], height/2, cost[i]/10);
+
+  var d = int(dist(mouseX, mouseY, positionX, positionY));
+                        if (d < 8){
+                                text('COST:', 50, 500);
+                                text(cost, 175, 500);
+                                text('COUNTY:', 50, 515);
+                                text(county, 175, 515);
+                                text('TIME:', 50, 530);
+                                text(time, 175, 530);
+                                noStroke();
+                        }
+}
+  }
