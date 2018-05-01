470a8a771010f64e301e6c3d5445107998023d93
EAD2187 <ead2187@columbia.edu>
2018-05-01 02:05:50 -0400

    180501_commit to master update 1

--- ASSIGNMENT7.3/archive/final_prototype_fail.js
+++ ASSIGNMENT7.3/archive/final_prototype_fail.js
@@ -35,6 +35,7 @@ function draw(){
 console.log(cost.length);
   for (var i=0;i<cost.length; i++) {
     
+    if(county[i]=="Q") {
     fill(0,255,168,30);
     var Xaxis = map (time[i],0,1499,50,width-50);
     var radius = map (cost[i],0,515,0,50);
@@ -44,5 +45,21 @@ console.log(cost.length);
     rotate(PI/3);
     line(0, -1*radius*4, 0, radius*4);
     pop();
+    }
+
+    console.log(county[i]);
+
+    if(county[i]=="NY") {
+    console.log("new york");
+    var Xaxis = map (time[i],0,1499,50,width-50);
+    var radius = map (cost[i],0,515,0,50);
+    stroke(255,0,0,10);
+    push();
+    translate(Xaxis,height/2);
+    rotate(PI/3);
+    line(0, -1*radius*4, 0, radius*4);
+    pop();
+
+    }
   }
 }
